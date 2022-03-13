9ca2840ed60175bdda1fc43666b93dce6cc9a3a6
Adrian Irving-Beer <wisq@wisq.net>
2022-03-13 16:09:24 -0400

    `mix new sem_solver`

---
+++ lib/sem_solver.ex
@@ -0,0 +1,18 @@
+defmodule SemSolver do
+  @moduledoc """
+  Documentation for `SemSolver`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SemSolver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sem_solver_test.exs
@@ -0,0 +1,8 @@
+defmodule SemSolverTest do
+  use ExUnit.Case
+  doctest SemSolver
+
+  test "greets the world" do
+    assert SemSolver.hello() == :world
+  end
+end
